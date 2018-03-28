68d10ef7b142f2e14213a2b3e72eea8bd649bae5
luis <luis@MacBook-de-luis.local>
2018-03-28 05:33:02 +0200

    Initial Commit

---
+++ SwiftDockerUI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftDockerUI
+//
+//  Created by luis on 28/3/18.
+//  Copyright © 2018 Talayot. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftDockerUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftDockerUI
+//
+//  Created by luis on 28/3/18.
+//  Copyright © 2018 Talayot. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
